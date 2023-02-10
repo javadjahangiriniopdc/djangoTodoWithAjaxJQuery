a8a49712c490a0c1192c5b4904d56cd1008ac09c
javad jahangiri <javad.jahangiri.niopdc@gmail.com>
2023-02-10 22:02:01 +0330

    add ajax get

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from core import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('todo-list/', views.todo_list, name='todo-list')
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from core.models import TODO
+from django.http import JsonResponse
+
+
 def home(request):
-    return render(request,'core/todo.html')
+    return render(request, 'core/todo.html')
+
+
+def todo_list(request):
+    todos = TODO.objects.all()
+    return JsonResponse({'todos':list(todos.values())})
