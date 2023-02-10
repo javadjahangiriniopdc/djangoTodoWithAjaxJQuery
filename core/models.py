f020e1f1c98545e573f8bc984f4e7e9ded69a7d3
javad jahangiri <javad.jahangiri.niopdc@gmail.com>
2023-02-10 21:47:35 +0330

    add TODO Model and migration and migrate

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class TODO(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
